aa7459278ff4fb9a896bdf339c2a4d126c3e1507
wancat <wancat@wancat.cc>
2024-05-19 20:45:40 +0800

    Add ImageSelector component to ToolBar

---
+++ src/components/ImageSelector.tsx
@@ -0,0 +1,10 @@
+import ToolButton from "./ToolButton";
+import { faFileImage } from "@fortawesome/free-solid-svg-icons";
+
+export default function ImageSelector() {
+  return (
+    <>
+      <ToolButton icon={faFileImage} />
+    </>
+  );
+}
--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -2,14 +2,14 @@ import {
   faBorderAll,
   faRuler,
   faMagnifyingGlass,
-  faFileImage,
 } from "@fortawesome/free-solid-svg-icons";
 import ToolButton from "./ToolButton";
+import ImageSelector from "./ImageSelector";
 
 export default function ToolBar() {
   return (
     <div className="fixed bottom-0 left-0 right-0 flex flex-row gap-3 py-2 items-center bg-white opacity-50 rounded-md mx-auto w-fit px-5 mb-5 hover:opacity-90 transition duration-100">
-      <ToolButton icon={faFileImage} />
+      <ImageSelector />
       <div className="w-px h-6 bg-gray-400"></div>
       <ToolButton icon={faBorderAll} />
       <ToolButton icon={faRuler} />
