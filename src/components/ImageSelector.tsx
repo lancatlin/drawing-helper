3233d94f9f4e2aede5aec8bc5d1516b708e6933b
wancat <wancat@wancat.cc>
2024-05-19 20:50:05 +0800

    Refactor ImageSelector component to use FontAwesomeIcon and add file input

--- src/components/ImageSelector.tsx
+++ src/components/ImageSelector.tsx
@@ -1,10 +1,13 @@
-import ToolButton from "./ToolButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileImage } from "@fortawesome/free-solid-svg-icons";
 
 export default function ImageSelector() {
   return (
     <>
-      <ToolButton icon={faFileImage} />
+      <label className="hover:scale-150 transition duration-100 cursor-pointer">
+        <input type="file" className="hidden" />
+        <FontAwesomeIcon icon={faFileImage} />
+      </label>
     </>
   );
 }
