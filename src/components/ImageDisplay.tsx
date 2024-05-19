54588800e0124ce9084bd0f9c5f62dd49a235798
wancat <wancat@wancat.cc>
2024-05-19 21:00:50 +0800

    feat: display selected image

--- src/components/ImageDisplay.tsx
+++ src/components/ImageDisplay.tsx
@@ -5,12 +5,17 @@ export default function ImageDisplay() {
   const imageSrc = useSelector(selectImage);
   return (
     <div className="fixed top-0 left-0 w-full h-full -z-10">
-      <p>{imageSrc}</p>
-      <img
-        src="../../public/leopard.jpg"
-        alt="image display"
-        className="w-full h-full object-contain"
-      />
+      {imageSrc ? (
+        <img
+          src={imageSrc || ""}
+          alt="image display"
+          className="w-full h-full object-contain"
+        />
+      ) : (
+        <div className="w-full h-full flex items-center justify-center">
+          <p className="text-2xl">No image selected. Open one to view it.</p>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/ImageSelector.tsx
+++ src/components/ImageSelector.tsx
@@ -1,11 +1,30 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileImage } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { setImage } from "../features/image/imageSlice";
 
 export default function ImageSelector() {
+  const dispatch = useDispatch();
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const [file] = e.target.files;
+      console.log(file);
+      if (file) {
+        const objectURL = window.URL.createObjectURL(file);
+        console.log(objectURL);
+        dispatch(setImage(objectURL));
+      }
+    }
+  };
   return (
     <>
       <label className="hover:scale-150 transition duration-100 cursor-pointer">
-        <input type="file" className="hidden" />
+        <input
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={onChange}
+        />
         <FontAwesomeIcon icon={faFileImage} />
       </label>
     </>
